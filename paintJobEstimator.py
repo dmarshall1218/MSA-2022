9d5ee1f94d3be98cd52c19abebf86080d13737b3
dmarshall1218 <107645601+dmarshall1218@users.noreply.github.com>
2022-06-17 14:41:39 -0500

    Update paintJobEstimator.py

--- paintJobEstimator.py
+++ paintJobEstimator.py
@@ -1,3 +1,20 @@
+import math
+#Function decleration
+def get_float_value(prompt):
+    run_again = True
+    while(run_again):
+        try:
+            user_input = float(input(prompt))
+            if(user_input <= 0):
+                print("ERROR: Value must be a positive number.\n")
+                continue
+        except:
+            print("ERROR: Input must be a number.\n")
+        else:
+            run_again = False
+
+    return user_input
+
 #INPUT
 #Declare variables for known values
 hourly_labor_cost = 62.25
@@ -6,35 +23,29 @@ hours_labor_per_unit = 6
 #prompt the user to enter the amount of wall to paint
 #convert into float value
 #if error in input ask user to reenter input, input must be greater than 0
-run_again = True
-while(run_again):
-    try:
-        wall_area = float(input("What is the area of wall in sq/ft: "))
-        if(wall_area <= 0):
-            continue
-    except:
-        print("ERROR: Wall area must be a number.\n")
-    else:
-        run_again = False
+wall_area = get_float_value("What is the area of the wall in sq/ft? ")
 #prompt user to enter cost of paint per gallon
-run_again = True
-while(run_again):
-    try:
-        paint_price = float(input("What is the price of paint per gallon: "))
-        if(paint_price <= 0):
-            continue
-    except:
-        print("ERROR: Paint prices must be a number.\n")
-    else:
-        run_again = False
+paint_price = get_float_value("What is the price of paint per gallon? ")
 
 #PROCESS
 #Calculate to hours of labor
+hours_of_labor = wall_area / unit_of_wall_area * hours_labor_per_unit
+
 #Calculate the cost of labor
+cost_of_labor = hours_of_labor * hourly_labor_cost
+
 #Calculate the amount of paint
+gallons_of_paint = math.ceil(wall_area / unit_of_wall_area)
+
 #Calculate the cost of the paint
+cost_of_paint = gallons_of_paint * paint_price
+
 #Calculate total cost of the job
+total_cost = cost_of_labor + cost_of_paint
 
 #OUTPUT
 #Print hours of labor, cost of labor, amount of paint, 
+print(f"Hours of labor: {hours_of_labor}\nCost of labor: {cost_of_labor}\nAmount of paint (gallons): {gallons_of_paint}")
+
 #cost of paint, total job cost
+print(f"Cost of paint: {cost_of_paint}\nTotal job cost: {total_cost}")
